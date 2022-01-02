2500fa275f6c7aeabe398e03a3affc337c23a7e3
aergesrg <88936731+aergesrg@users.noreply.github.com>
2022-01-02 22:46:59 +0300

    done

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 const burger = document.querySelector('#burger');
 const mobMenu = document.querySelector('.mob-nav');
+const body = document.querySelector('body')
+const bg = document.querySelector('.back-for-burger-menu')
 burger.onclick = function(){
     mobMenu.classList.toggle("active");
+    body.classList.toggle("scroll-block");
+    bg.classList.toggle("activ-bg");
+}
+bg.onclick = function(){
+    mobMenu.classList.remove("active");
+    body.classList.remove("scroll-block");
+    bg.classList.remove("activ-bg");
 }
